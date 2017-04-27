7c249afd9c65f638b39cc789df0f41b9e38dc764
Barry Sevig <barrysevig@seviglius.domain>
2017-04-27 07:30:13 -0700

    sample changes for splash

---
+++ front-page.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: front-page
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Portshowlio 2017</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT AWESOME
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+
+
+  <?php wp_head(); ?>
+</head>
+<body>
+
+hello world
+<ul>
+  <li>one</li>
+  <li>two</li>
+</ul> 
+
+<?php wp_footer(); ?>
+
+<!-- Scripts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<!--  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script> -->
+<!-- <script src="js/sccawaves.js"></script> -->
+
+
+
+</body>
+</html>
